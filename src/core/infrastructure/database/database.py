a65707391b739b0ad5772ddd1bd8e674facabf2d
Petr Novotný <krkabol@gmail.com>
2026-04-13 12:30:16 +0200

    hespi db select preliminary

--- src/core/infrastructure/database/database.py
+++ src/core/infrastructure/database/database.py
@@ -5,12 +5,30 @@ class Database(BaseDatabase):
     def fetch_records(self, databot_id: int, limit: int = 1000):
         return self.fetchall(
             """
-            SELECT p.id, databot_thumb_filename
+            SELECT p.id, bucket_suffix, databot_thumb_filename
             FROM photos p
                      LEFT JOIN databots.databot_results dr
                                ON dr.photo_id = p.id
                                    AND dr.databot_id = %s
             WHERE p.status_id > 1 -- skip waiting/unprocessed
+              AND dr.id IS NULL
+            ORDER BY p.id ASC
+                LIMIT %s
+            """,
+            (databot_id, limit)
+        )
+
+    # use for Hespi&comp
+    def fetch_specimen_type_records(self, databot_id: int, limit: int = 1000):
+        return self.fetchall(
+            """
+            SELECT p.id, bucket_suffix, databot_thumb_filename
+            FROM photos p
+                     LEFT JOIN databots.databot_results dr
+                               ON dr.photo_id = p.id
+                                   AND dr.databot_id = %s
+            WHERE p.status_id > 1 -- skip waiting/unprocessed
+              AND p.type_id = 1   -- only "Specimen" type of photos
               AND dr.id IS NULL
             ORDER BY p.id ASC
                 LIMIT %s
